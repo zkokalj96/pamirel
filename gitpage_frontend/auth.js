d32bda0dfd877b9caf6b66c67df94534dcd9fb15
rubi <rubi.zelenik@gmail.com>
2025-07-28 09:13:00 +0200

    Add files via upload

---
+++ gitpage_frontend/auth.js
@@ -0,0 +1,79 @@
+
+// Handles logins, checks if id and password inputs have been given, if not gives an alert
+// and if they are valid, sends a message to the backend
+function login() {
+  const id = document.getElementById('esp_id').value.trim();
+  const pass = document.getElementById('password').value;
+
+  if (!id || !pass) {
+    alert("Please enter ID and password");
+    return;
+  }
+
+  // Initialising websocket communication with the url where its hosted
+  ws = new WebSocket("ws://localhost:300/socket/"); //"wss://raspiwebsocket.duckdns.org/socket/"
+
+  // When connection is established sends the credentials to check if they are valid
+  ws.onopen = () => {
+    ws.send(JSON.stringify({
+      type: "login",
+      esp_id: id,
+      password: pass
+    }));
+  };
+
+  // When a message is received, checks if it is a login message and if it was successful
+  // If it was, displays the thermostat state and options to view history or set temperature
+  ws.onmessage = (recievedMessage) => {
+    const message = JSON.parse(recievedMessage.data);
+    if (message.type === "login" && message.success) {
+      const latestData = message.latest_data;
+      
+      const state = (latestData.status_on == 1) ? `ON, and it is ${(latestData.heating) ? "heating" : "cooling"}` : "OFF";
+      
+      document.getElementById('login_successful').innerHTML = `
+      <div class="card">
+        <h2>Login successful!</h2>
+        <h3>Thermostat ${id} is currently ${state} </h3>
+          <p>Current Temperature: ${latestData.temp} °C</p>
+          <p>Set Temperature: ${latestData.set_temp} °C</p>
+          <p>Pressure: ${latestData.pressure} kPa</p>
+          <p>Ventilator speed: ${latestData.ventilator} / 3</p>
+          <p>Set Ventilator speed: ${latestData.set_ventilator} / 3</p>
+          <p>At ${formatDateForUser(latestData.recorded)}</p>
+        <div class="actions">
+          <button onclick="historyOptions()">Show History</button>
+          <button onclick="setTemp()">Set Temperature</button>
+        </div>
+      </div>
+      `;
+    } else {
+      document.getElementById('login_successful').innerHTML = `
+      <div class="card">
+        <h2>Login failed!</h2>
+        <p>Double-check ID and password.</p>
+      </div>
+      `;}
+  };
+
+  ws.onerror = (err) => {
+    console.error("WebSocket error", err);
+    alert("Connection error, try again later");
+  };
+}
+
+// Formats MySQL timestamps into '{time} on {date}' strings
+function formatDateForUser(mysqlTimestamp) {
+  const date = new Date(mysqlTimestamp);
+
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  const day = date.getDate();
+  const month = date.getMonth() + 1; // Months are 0-indexed
+  const year = date.getFullYear();
+
+  const formatted = `${hours}:${minutes}:${seconds} on ${day}. ${month}. ${year}`;
+  return formatted;
+}
---
+++ gitpage_frontend/settings.js
@@ -0,0 +1,74 @@
+// Function which displays an html form to set a new temperature
+function setTemp() {
+  document.getElementById('setting_temperature').innerHTML = `
+    <div class="settings card">
+      <h2>Set Temperature</h2>
+      <div class="rules">
+        <h4>Temperature should be:</h4>
+        <ul>
+          <li><p>In the range 10°C - 30°C</p></li>
+          <li><p>Set to no more than one decimal place (<del>14.22</del> 14.2 <i style="font-size:20px" class="fa">&#10004;</i> ) </p></li>
+        </ul>
+      </div>
+      <p class="description">Enter the desired temperature:</p>
+      <input type="number" id="new_temp" placeholder="Temperature in °C" />
+      <button onclick="submitTemp()">Submit</button>
+
+      <p id="setting_successful"></p>
+
+      <button id="hideButton" onclick="hideSettings()">Hide Settings</button>
+    </div>
+  `;
+}
+
+/* 
+Function which submits the new temperature to the ESP via the WebSocket server
+It checks if the input is valid before sending it
+After recieveing conformation of the change, it displays some text to the website
+Alternatively, it displays the error and reason
+*/
+function submitTemp() {
+  const newTemp = document.getElementById('new_temp').value.trim();
+  const id = document.getElementById('esp_id').value.trim();
+  ws = new WebSocket("ws://localhost:300/socket/"); // "wss://raspiwebsocket.duckdns.org/socket/"
+
+  if (!newTemp) {
+    alert("Please enter a temperature");
+    return;
+  }
+  // Check if the input is a valid number and within the expected range
+  else if (isNaN(newTemp) || newTemp < 10 || newTemp > 30) {
+    alert("Please enter a valid temperature between 0 and 30 °C");
+    return;
+  }
+  // Check if the input has no more than one decimal place
+  else if (newTemp % 1 !== 0 && newTemp % 1 !== 0.1) {
+    alert("Please enter a temperature with no more than one decimal place");
+    return;
+  }
+
+  ws.onopen = () => {
+    console.log("sent the new temperature to the ESP");
+    ws.send(JSON.stringify({
+      type: "changing_set_temp",
+      esp_id: id,
+      new_temp: parseFloat(newTemp)
+    }));
+  };
+
+  ws.onmessage = (recievedMessage) => {
+    const message = JSON.parse(recievedMessage.data);
+    if (message.type === "changing_set_temp" && message.success) {
+      document.getElementById("setting_successful").innerHTML = `
+      <h4>Setting changes stored successfully!</h4>
+      `;
+    } else {
+      alert(message.reason)
+    }
+  };
+}
+
+// Function to hide the settings form
+function hideSettings() {
+  document.getElementById('setting_temperature').innerHTML = '';
+}
