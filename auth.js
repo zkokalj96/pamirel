f41b152284c6fefcf4614422a6888ecea7d99bad
rubi <rubi.zelenik@gmail.com>
2025-12-09 15:08:28 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -11,7 +11,7 @@ function login() {
   }
 
   // Initialising websocket communication with the url where its hosted
-  ws = new WebSocket("ws://localhost:300/socket/");
+  ws = new WebSocket("wss://raspiwebsocket.duckdns.org/socket/");
 
 
   // When connection is established sends the credentials to check if they are valid
